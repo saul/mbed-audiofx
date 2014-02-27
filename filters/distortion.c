783202bed22bb8069e8b08ed530a83f918deb66b
Tom Bryant <me@tombryant.co.uk>
2014-02-27 13:05:13 +0000

    Bitcrusher effect added

--- filters/distortion.c
+++ filters/distortion.c
@@ -2,3 +2,18 @@
 #include "dbg.h"
 #include "samples.h"
 #include "distortion.h"
+
+
+uint32_t filter_bitcrusher_apply(uint32_t input, void *pUnknown)
+{
+	const FilterBitcrusherData_t *pData = (const FilterBitcrusherData_t *)pUnknown;
+
+	return (input >> pData->bitLoss) << pData->bitLoss;
+}
+
+
+void filter_bitcrusher_debug(void *pUnknown)
+{
+	const FilterBitcrusherData_t *pData = (const FilterBitcrusherData_t *)pUnknown;
+	dbg_printf("bitLoss=%u", pData->bitLoss);
+}
--- filters/distortion.h
+++ filters/distortion.h
@@ -3,12 +3,12 @@
 
 typedef struct
 {
-	uint16_t sensitivity;
-	uint16_t threshold;
+	uint8_t bitLoss;	// between 0 and 10: number of bits of sensitivity to lose
+	uint8_t sampleLoss;	// Number of samples to lose (divisor of BUFFER_SAMPLES)
 } FilterBitcrusherData_t;
 
 
-uint32_t filter_noisegate_apply(uint32_t input, void *pPrivate);
-void filter_noisegate_debug(void *pPrivate);
+uint32_t filter_bitcrusher_apply(uint32_t input, void *pUnknown);
+void filter_bitcrusher_debug(void *pUnknown);
 
 #endif
