cecd8557b3bf95485de0a1a3888adc7f9bc65ca1
Saul Rennison <saul.rennison@gmail.com>
2014-03-04 17:35:20 +0000

    Updated analog control checkbox

--- ui/Resources/js/ui.js
+++ ui/Resources/js/ui.js
@@ -191,14 +191,12 @@ $(document).on('change', '.form-group[data-param-name]', $.debounce(250, functio
 
 /* Tom individual */
 function updateAnalogControls(new_value) {
-	$('.checkbox').forEach(function(entry) {
-				if(entry.prop('checked')) {
-					var control_element = $('[name="'+entry.attr('name').replace('-ac', '')+'"]');
-					var min = control_element.attr('min');
-					var max = control_element.attr('max');
-					new_value = min + (max-min)*new_value;
-					control_element.val(new_value);
-				}
+	$('[name$=-ac]:checked').each(function(entry) {
+		var control_element = $('[name="'+entry.attr('name').replace('-ac', '')+'"]');
+		var min = control_element.attr('min');
+		var max = control_element.attr('max');
+		new_value = min + (max-min)*new_value;
+		control_element.val(new_value);
 	});
 }
 /* End Tom individual */
